c6511645c283b20a82f474b9adeae8914b45eb48
victorprad <victor@robots.ox.ac.uk>
2014-10-15 14:42:57 +0100

    fixed small memory leak (wasn't deleting ITMVisualisationEngine)

--- InfiniTAM/ITMLib/Engine/ITMMainEngine.cpp
+++ InfiniTAM/ITMLib/Engine/ITMMainEngine.cpp
@@ -27,6 +27,7 @@ ITMMainEngine::ITMMainEngine(const ITMLibSettings *settings, const ITMRGBDCalib 
 		else tracker = new ITMColorTracker_CUDA(imgSize, settings->noHierarchyLevels, settings->noRotationOnlyLevels, lowLevelEngine);
 
 		if (settings->useSwapping) swappingEngine = new ITMSwappingEngine_CUDA<ITMVoxel,ITMVoxelIndex>();
+
 		visualisationEngine = new ITMVisualisationEngine_CUDA<ITMVoxel,ITMVoxelIndex>();
 #endif
 	}
@@ -41,6 +42,7 @@ ITMMainEngine::ITMMainEngine(const ITMLibSettings *settings, const ITMRGBDCalib 
 		else tracker = new ITMColorTracker_CPU(imgSize, settings->noHierarchyLevels, settings->noRotationOnlyLevels, lowLevelEngine);
 
 		if (settings->useSwapping) swappingEngine = new ITMSwappingEngine_CPU<ITMVoxel,ITMVoxelIndex>();
+
 		visualisationEngine = new ITMVisualisationEngine_CPU<ITMVoxel,ITMVoxelIndex>();
 	}
 
@@ -56,6 +58,11 @@ ITMMainEngine::~ITMMainEngine()
 	delete lowLevelEngine;
 
 	if (settings->useSwapping) delete swappingEngine;
+
+	delete visualisationEngine;
+
+	if (visualisationState != NULL) 
+		delete visualisationState;
 
 	delete trackingState;
 	delete scene;
