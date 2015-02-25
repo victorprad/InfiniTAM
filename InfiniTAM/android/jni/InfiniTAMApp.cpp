8a3b1c51b308e4cdd472e4776544f15a49477433
Olaf Kaehler <olaf@robots.ox.ac.uk>
2015-02-25 14:57:25 +0000

    fixed the display a bit, still has glitches...

--- InfiniTAM/android/jni/InfiniTAMApp.cpp
+++ InfiniTAM/android/jni/InfiniTAMApp.cpp
@@ -53,7 +53,7 @@ __android_log_print(ANDROID_LOG_VERBOSE, "InfiniTAM", "ResizeGL: %i %i", newWidt
 	Vector2i depthImageSize = mImageSource->getDepthImageSize();
 	float ratio = (float)depthImageSize.x/(float)depthImageSize.y;
 
-	glViewport(0, 0, newWidth, newHeight);
+//	glViewport(0, 0, newWidth, newHeight);
 	if (newWidth >= newHeight) {
 		winPos[0] = Vector4f(0.0f, 0.0f, 1.0f/ratio, 1.0f);
 		winPos[1] = Vector4f(1.0f/ratio, 0.5f, 1.0f, 1.0f);
@@ -68,46 +68,45 @@ __android_log_print(ANDROID_LOG_VERBOSE, "InfiniTAM", "ResizeGL: %i %i", newWidt
 void InfiniTAMApp::RenderGL(void)
 {
 __android_log_print(ANDROID_LOG_VERBOSE, "InfiniTAM", "RenderGL");
+
+	int localNumWin = 1;//NUM_WIN
+	for (int w = 0; w < localNumWin; w++) mMainEngine->GetImage(outImage[w], winImageType[w], false);
+
 	glClear(GL_COLOR_BUFFER_BIT);
-//	glColor3f(1.0f, 1.0f, 1.0f);
 
 	glMatrixMode(GL_PROJECTION);
 	glPushMatrix();
 	{
 		glLoadIdentity();
-		glOrthof(0.0, 1.0, 0.0, 1.0, 0.0, 1.0);
+		glOrthof(0.0f, 1.0f, 0.0f, 1.0f, -1.0f, 1.0f);
 
 		glMatrixMode(GL_MODELVIEW);
 		glPushMatrix();
 		{
 			glEnable(GL_TEXTURE_2D);
+			glDisable(GL_BLEND);
 			glEnableClientState(GL_VERTEX_ARRAY);
 			glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-			for (int w = 0; w < NUM_WIN; w++) {
-
+			for (int w = 0; w < localNumWin; w++) {
 				glBindTexture(GL_TEXTURE_2D, textureId[w]);
 				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, outImage[w]->noDims.x, outImage[w]->noDims.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, outImage[w]->GetData(MEMORYDEVICE_CPU));
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
 				float vertices[] = {
-					winPos[w][0], winPos[w][1],
-					winPos[w][2], winPos[w][1],
 					winPos[w][0], winPos[w][3],
-					winPos[w][2], winPos[w][3] };
+					winPos[w][2], winPos[w][3],
+					winPos[w][0], winPos[w][1],
+					winPos[w][2], winPos[w][1] };
 				float texture[] = {
 					0.0f, 0.0f,
 					1.0f, 0.0f,
 					0.0f, 1.0f,
 					1.0f, 1.0f };
-				unsigned char indices[] = {
-					0, 1, 3,
-					0, 3, 2 };
 
 				glVertexPointer(2, GL_FLOAT, 0, vertices);
 				glTexCoordPointer(2, GL_FLOAT, 0, texture);
-				glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, indices);
-
+				glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 			}
 			glDisableClientState(GL_VERTEX_ARRAY);
 			glDisableClientState(GL_TEXTURE_COORD_ARRAY);
@@ -117,8 +116,6 @@ __android_log_print(ANDROID_LOG_VERBOSE, "InfiniTAM", "RenderGL");
 	}
 	glMatrixMode(GL_PROJECTION);
 	glPopMatrix();
-
-	glFlush();
 }
 
 bool InfiniTAMApp::ProcessFrame(void)
