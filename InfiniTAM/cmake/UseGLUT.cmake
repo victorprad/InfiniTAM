b3358033cf4190c9e6535607ce288288f75e055d
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-10-29 12:21:40 +0000

    Fix some build problems.

--- InfiniTAM/cmake/UseGLUT.cmake
+++ InfiniTAM/cmake/UseGLUT.cmake
@@ -3,8 +3,9 @@
 ###################
 
 IF(MSVC_IDE)
-  FIND_PATH(GLUT_INCLUDE_DIR GL/glut.h HINTS "$ENV{HOMEPATH}/Downloads/freeglut/include")
-  FIND_LIBRARY(GLUT_LIBRARY freeglut HINTS "$ENV{HOMEPATH}/Downloads/freeglut/lib/x64")
+  FIND_PATH(GLUT_ROOT Readme.txt HINTS "$ENV{HOMEPATH}/Downloads/freeglut")
+  FIND_PATH(GLUT_INCLUDE_DIR GL/glut.h HINTS "${GLUT_ROOT}/include")
+  FIND_LIBRARY(GLUT_LIBRARY freeglut HINTS "${GLUT_ROOT}/lib/x64")
 ENDIF()
 
 INCLUDE_DIRECTORIES(${GLUT_INCLUDE_DIR})
