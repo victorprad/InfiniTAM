aeb54935ffec2de1909ce73016b8cd986a637880
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-10-30 17:59:41 +0000

    Continue adding the necessary files for surfel-based fusion.

---
+++ InfiniTAM/ITMLib/Engine/DeviceSpecific/CUDA/ITMSurfelSceneReconstructionEngine_CUDA.h
@@ -0,0 +1,26 @@
+// Copyright 2014-2015 Isis Innovation Limited and the authors of InfiniTAM
+
+#pragma once
+
+#include "../../ITMSurfelSceneReconstructionEngine.h"
+
+namespace ITMLib
+{
+  /**
+   * \brief An instance of an instantiation of this class template can be used to make a surfel-based reconstruction of a 3D scene using CUDA.
+   */
+  template <typename TSurfel>
+  class ITMSurfelSceneReconstructionEngine_CUDA : public ITMSurfelSceneReconstructionEngine<TSurfel>
+  {
+    //#################### PUBLIC MEMBER FUNCTIONS ####################
+  public:
+    /** Override */
+    virtual void AllocateSceneFromDepth(ITMSurfelScene<TSurfel> *scene, const ITMView *view, const ITMTrackingState *trackingState) const;
+
+    /** Override */
+    virtual void IntegrateIntoScene(ITMSurfelScene<TSurfel> *scene, const ITMView *view, const ITMTrackingState *trackingState) const;
+
+    /** Override */
+    virtual void ResetScene(ITMSurfelScene<TSurfel> *scene) const;
+  };
+}
