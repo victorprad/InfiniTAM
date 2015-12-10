bc03dec9fb8e92255d248bf613a659c90a4d8cdc
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-12-10 16:20:13 +0000

    Implement the CUDA version of MakeIndexImage.

--- InfiniTAM/ITMLib/Engine/DeviceSpecific/CUDA/ITMSurfelVisualisationEngine_CUDA.cu
+++ InfiniTAM/ITMLib/Engine/DeviceSpecific/CUDA/ITMSurfelVisualisationEngine_CUDA.cu
@@ -9,6 +9,15 @@ namespace ITMLib
 
 //#################### CUDA KERNELS ####################
 
+__global__ void ck_clear_surfel_index_image(int pixelCount, unsigned int *surfelIndexImage, int *depthBuffer)
+{
+  int locId = threadIdx.x + blockDim.x * blockIdx.x;
+  if(locId < pixelCount)
+  {
+    clear_surfel_index_image(locId, surfelIndexImage, depthBuffer);
+  }
+}
+
 #if DEBUG_CORRESPONDENCES
 template <typename TSurfel>
 __global__ void ck_copy_correspondences_to_buffer(int surfelCount, const TSurfel *surfels, float *correspondences)
@@ -28,6 +37,17 @@ __global__ void ck_copy_scene_to_buffers(int surfelCount, const TSurfel *surfels
   if(surfelId < surfelCount)
   {
     copy_surfel_to_buffers(surfelId, surfels, positions, normals, colours);
+  }
+}
+
+template <typename TSurfel>
+__global__ void ck_project_to_surfel_index_image(int surfelCount, const TSurfel *surfels, Matrix4f invT, ITMIntrinsics intrinsics, int indexImageWidth, int indexImageHeight,
+                                                 int scaleFactor, unsigned int *surfelIndexImage, int *depthBuffer)
+{
+  int surfelId = threadIdx.x + blockDim.x * blockIdx.x;
+  if(surfelId < surfelCount)
+  {
+    project_to_surfel_index_image(surfelId, surfels, invT, intrinsics, indexImageWidth, indexImageHeight, scaleFactor, surfelIndexImage, depthBuffer);
   }
 }
 
@@ -86,7 +106,31 @@ template <typename TSurfel>
 void ITMSurfelVisualisationEngine_CUDA<TSurfel>::MakeIndexImage(const ITMSurfelScene<TSurfel> *scene, const ITMPose *pose, const ITMIntrinsics *intrinsics,
                                                                 int width, int height, int scaleFactor, unsigned int *surfelIndexImage, int *depthBuffer) const
 {
-  // TODO
+  const int pixelCount = width * height;
+
+  int threadsPerBlock = 256;
+  int numBlocks = (pixelCount + threadsPerBlock - 1) / threadsPerBlock;
+
+  ck_clear_surfel_index_image<<<numBlocks,threadsPerBlock>>>(
+    pixelCount,
+    surfelIndexImage,
+    depthBuffer
+  );
+
+  const int surfelCount = static_cast<int>(scene->GetSurfelCount());
+  numBlocks = (surfelCount + threadsPerBlock - 1) / threadsPerBlock;
+
+  ck_project_to_surfel_index_image<<<numBlocks,threadsPerBlock>>>(
+    surfelCount,
+    scene->GetSurfels()->GetData(MEMORYDEVICE_CUDA),
+    pose->GetM(),
+    *intrinsics,
+    width,
+    height,
+    scaleFactor,
+    surfelIndexImage,
+    depthBuffer
+  );
 }
 
 //#################### EXPLICIT INSTANTIATIONS ####################
