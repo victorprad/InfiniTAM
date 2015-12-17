21bb62bd58cac41d009be3ccdf446c1a77c1197f
Stuart Golodetz <sgolodetz@gxstudios.net>
2015-12-17 17:40:39 +0000

    Move the voxel types into a separate file.

---
+++ InfiniTAM/ITMLib/Utils/ITMVoxelTypes.h
@@ -0,0 +1,109 @@
+// Copyright 2014-2015 Isis Innovation Limited and the authors of InfiniTAM
+
+#pragma once
+
+#include "ITMMath.h"
+
+/** \brief
+    Stores the information of a single voxel in the volume
+*/
+struct ITMVoxel_f_rgb
+{
+	_CPU_AND_GPU_CODE_ static float SDF_initialValue() { return 1.0f; }
+	_CPU_AND_GPU_CODE_ static float SDF_valueToFloat(float x) { return x; }
+	_CPU_AND_GPU_CODE_ static float SDF_floatToValue(float x) { return x; }
+
+	static const CONSTPTR(bool) hasColorInformation = true;
+
+	/** Value of the truncated signed distance transformation. */
+	float sdf;
+	/** Number of fused observations that make up @p sdf. */
+	uchar w_depth;
+	/** RGB colour information stored for this voxel. */
+	Vector3u clr;
+	/** Number of observations that made up @p clr. */
+	uchar w_color;
+
+	_CPU_AND_GPU_CODE_ ITMVoxel_f_rgb()
+	{
+		sdf = SDF_initialValue();
+		w_depth = 0;
+		clr = (uchar)0;
+		w_color = 0;
+	}
+};
+
+/** \brief
+    Stores the information of a single voxel in the volume
+*/
+struct ITMVoxel_s_rgb
+{
+	_CPU_AND_GPU_CODE_ static short SDF_initialValue() { return 32767; }
+	_CPU_AND_GPU_CODE_ static float SDF_valueToFloat(float x) { return (float)(x) / 32767.0f; }
+	_CPU_AND_GPU_CODE_ static short SDF_floatToValue(float x) { return (short)((x) * 32767.0f); }
+
+	static const CONSTPTR(bool) hasColorInformation = true;
+
+	/** Value of the truncated signed distance transformation. */
+	short sdf;
+	/** Number of fused observations that make up @p sdf. */
+	uchar w_depth;
+	/** Padding that may or may not improve performance on certain GPUs */
+	//uchar pad;
+	/** RGB colour information stored for this voxel. */
+	Vector3u clr;
+	/** Number of observations that made up @p clr. */
+	uchar w_color;
+
+	_CPU_AND_GPU_CODE_ ITMVoxel_s_rgb()
+	{
+		sdf = SDF_initialValue();
+		w_depth = 0;
+		clr = (uchar)0;
+		w_color = 0;
+	}
+};
+
+struct ITMVoxel_s
+{
+	_CPU_AND_GPU_CODE_ static short SDF_initialValue() { return 32767; }
+	_CPU_AND_GPU_CODE_ static float SDF_valueToFloat(float x) { return (float)(x) / 32767.0f; }
+	_CPU_AND_GPU_CODE_ static short SDF_floatToValue(float x) { return (short)((x) * 32767.0f); }
+
+	static const CONSTPTR(bool) hasColorInformation = false;
+
+	/** Value of the truncated signed distance transformation. */
+	short sdf;
+	/** Number of fused observations that make up @p sdf. */
+	uchar w_depth;
+	/** Padding that may or may not improve performance on certain GPUs */
+	//uchar pad;
+
+	_CPU_AND_GPU_CODE_ ITMVoxel_s()
+	{
+		sdf = SDF_initialValue();
+		w_depth = 0;
+	}
+};
+
+struct ITMVoxel_f
+{
+	_CPU_AND_GPU_CODE_ static float SDF_initialValue() { return 1.0f; }
+	_CPU_AND_GPU_CODE_ static float SDF_valueToFloat(float x) { return x; }
+	_CPU_AND_GPU_CODE_ static float SDF_floatToValue(float x) { return x; }
+
+	static const CONSTPTR(bool) hasColorInformation = false;
+
+	/** Value of the truncated signed distance transformation. */
+	float sdf;
+	/** Number of fused observations that make up @p sdf. */
+	uchar w_depth;
+	/** Padding that may or may not improve performance on certain GPUs */
+	//uchar pad;
+
+	_CPU_AND_GPU_CODE_ ITMVoxel_f()
+	{
+		sdf = SDF_initialValue();
+		w_depth = 0;
+	}
+};
