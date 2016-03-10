1d942acd4213aacbc1c4cacee0de95d77d86abe6
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-03-10 16:15:23 +0000

    Set up automatic copying of the FFmpeg DLLs.

--- InfiniTAM/cmake/LinkFFmpeg.cmake
+++ InfiniTAM/cmake/LinkFFmpeg.cmake
@@ -4,4 +4,15 @@
 
 IF(WITH_FFMPEG)
   TARGET_LINK_LIBRARIES(${targetname} ${FFMPEG_LIBRARIES})
+
+  IF(MSVC_IDE)
+    ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different "${FFmpeg_SHARED_ROOT}/bin/avcodec-57.dll" "$<TARGET_FILE_DIR:${targetname}>")
+    ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different "${FFmpeg_SHARED_ROOT}/bin/avdevice-57.dll" "$<TARGET_FILE_DIR:${targetname}>")
+    ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different "${FFmpeg_SHARED_ROOT}/bin/avfilter-6.dll" "$<TARGET_FILE_DIR:${targetname}>")
+    ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different "${FFmpeg_SHARED_ROOT}/bin/avformat-57.dll" "$<TARGET_FILE_DIR:${targetname}>")
+    ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different "${FFmpeg_SHARED_ROOT}/bin/avutil-55.dll" "$<TARGET_FILE_DIR:${targetname}>")
+    ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different "${FFmpeg_SHARED_ROOT}/bin/postproc-54.dll" "$<TARGET_FILE_DIR:${targetname}>")
+    ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different "${FFmpeg_SHARED_ROOT}/bin/swresample-2.dll" "$<TARGET_FILE_DIR:${targetname}>")
+    ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different "${FFmpeg_SHARED_ROOT}/bin/swscale-4.dll" "$<TARGET_FILE_DIR:${targetname}>")
+  ENDIF()
 ENDIF()
