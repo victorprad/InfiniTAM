736abd41a1b3cfc9a1018156186104dc249977ca
Olaf Kaehler <olaf@robots.ox.ac.uk>
2016-04-14 17:19:20 +0100

    revert to default settings

--- InfiniTAM/ITMLib/Utils/ITMLibSettings.cpp
+++ InfiniTAM/ITMLib/Utils/ITMLibSettings.cpp
@@ -29,7 +29,7 @@ ITMLibSettings::ITMLibSettings(void)
 	useSwapping = false;
 
 	/// enables or disables approximate raycast
-	useApproximateRaycast = true;
+	useApproximateRaycast = false;
 
 	/// enable or disable bilateral depth filtering;
 	useBilateralFilter = false;
@@ -40,10 +40,10 @@ ITMLibSettings::ITMLibSettings(void)
 	/// enable or disable tracking failure detection
 	useTrackingFailureDetection = false;
 
-	//trackerConfig = "type=icp,levels=rrrbb,minstep=1e-3,outlierC=0.01,outlierF=0.002,numiterC=10,numiterF=2,failureDec=3.0";
-//	trackerConfig = "type=extended,levels=rrbb,minstep=1e-4,outlierSpaceC=0.1,outlierSpaceF=0.004,numiterC=20,numiterF=20,tukeyCutOff=8,framesToSkip=20,framesToWeight=50,failureDec=20.0";
+	trackerConfig = "type=icp,levels=rrrbb,minstep=1e-3,outlierC=0.01,outlierF=0.002,numiterC=10,numiterF=2,failureDec=3.0";
+	//trackerConfig = "type=extended,levels=rrbb,minstep=1e-4,outlierSpaceC=0.1,outlierSpaceF=0.004,numiterC=20,numiterF=20,tukeyCutOff=8,framesToSkip=20,framesToWeight=50,failureDec=20.0";
 	//trackerConfig = "type=rgb,levels=rrrbb";
 	//trackerConfig = "type=ren,levels=bb";
 	//trackerConfig = "type=imuicp,levels=tb,minstep=1e-3,outlierC=0.01,outlierF=0.005,numiterC=4,numiterF=2";
-	trackerConfig = "type=extendedimu,levels=ttb,minstep=5e-4,outlierSpaceC=0.1,outlierSpaceF=0.004,numiterC=20,numiterF=5,tukeyCutOff=8,framesToSkip=20,framesToWeight=50,failureDec=20.0";
+	//trackerConfig = "type=extendedimu,levels=ttb,minstep=5e-4,outlierSpaceC=0.1,outlierSpaceF=0.004,numiterC=20,numiterF=5,tukeyCutOff=8,framesToSkip=20,framesToWeight=50,failureDec=20.0";
 }
