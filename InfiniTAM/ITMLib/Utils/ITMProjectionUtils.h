94ff7f1e309620f9c1f3d8547d59782095a9b496
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2016-08-10 12:57:32 +0100

    Tidied up casts

--- InfiniTAM/ITMLib/Utils/ITMProjectionUtils.h
+++ InfiniTAM/ITMLib/Utils/ITMProjectionUtils.h
@@ -17,14 +17,14 @@ template<typename T> _CPU_AND_GPU_CODE_ inline Vector2f project(const THREADPTR(
 
 template<typename T> _CPU_AND_GPU_CODE_ inline Vector3f reproject(const THREADPTR(T) &point_2d, const THREADPTR(float) depth, const THREADPTR(Vector4f) &intrinsics)
 {
-	return Vector3f(depth * ((float(point_2d.x) - intrinsics.z) / intrinsics.x),
-					depth * ((float(point_2d.y) - intrinsics.w) / intrinsics.y),
+	return Vector3f(depth * (((float)point_2d.x - intrinsics.z) / intrinsics.x),
+					depth * (((float)point_2d.y - intrinsics.w) / intrinsics.y),
 					depth);
 }
 
 template<typename T> _CPU_AND_GPU_CODE_ inline Vector3f reproject(const THREADPTR(T) x, const THREADPTR(T) y, const THREADPTR(float) depth, const THREADPTR(Vector4f) &intrinsics)
 {
-	return Vector3f(depth * ((float(x) - intrinsics.z) / intrinsics.x),
-					depth * ((float(y) - intrinsics.w) / intrinsics.y),
+	return Vector3f(depth * (((float)x - intrinsics.z) / intrinsics.x),
+					depth * (((float)y - intrinsics.w) / intrinsics.y),
 					depth);
 }
