76984fc4856c9b7f9c450ac7f0b5b395a9403fcf
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2016-08-11 14:18:10 +0100

    Update default parameters

--- InfiniTAM/ITMLib/Utils/ITMLibSettings.cpp
+++ InfiniTAM/ITMLib/Utils/ITMLibSettings.cpp
@@ -42,8 +42,8 @@ ITMLibSettings::ITMLibSettings(void)
 	behaviourOnFailure = FAILUREMODE_IGNORE;
     
 	//trackerConfig = "type=icp,levels=rrrbb,minstep=1e-3,outlierC=0.01,outlierF=0.002,numiterC=10,numiterF=2,failureDec=3.0";
-	trackerConfig = "type=extended,levels=bbb,useDepth=1,useColour=1,colourWeight=0.25,minstep=1e-4,"
-			"outlierColourC=0.15,outlierColourF=0.03,"
+	trackerConfig = "type=extended,levels=rbb,useDepth=1,useColour=1,colourWeight=0.1,minstep=1e-4,"
+			"outlierColourC=0.2,outlierColourF=0.04,"
 			"outlierSpaceC=0.1,outlierSpaceF=0.004,"
 			"numiterC=20,numiterF=50,tukeyCutOff=8,framesToSkip=20,framesToWeight=50,failureDec=20.0";
 	//trackerConfig = "type=rgb,levels=rrbb";
