42614c7a41f7f45d8398a374a46cf74c3a380659
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-08-16 14:57:25 +0100

    Tidy up a bit.

--- InfiniTAM/ITMLib/Core/MultiScene/ITMLocalScene.h
+++ InfiniTAM/ITMLib/Core/MultiScene/ITMLocalScene.h
@@ -37,7 +37,7 @@ namespace ITMLib {
 		ConstraintList relations;
 		ORUtils::SE3Pose estimatedGlobalPose;
 
-		ITMLocalScene(const ITMLibSettings *settings, const ITMVisualisationEngine<TVoxel,TIndex> *visualisationEngine, const Vector2i & trackedImageSize)
+		ITMLocalScene(const ITMLibSettings *settings, const Vector2i & trackedImageSize)
 		{
 			MemoryDeviceType memoryType = settings->deviceType == ITMLibSettings::DEVICE_CUDA ? MEMORYDEVICE_CUDA : MEMORYDEVICE_CPU;
 			scene = new ITMScene<TVoxel,TIndex>(&settings->sceneParams, settings->swappingMode == ITMLibSettings::SWAPPINGMODE_ENABLED, memoryType);
@@ -52,4 +52,3 @@ namespace ITMLib {
 		}
 	};
 }
-
