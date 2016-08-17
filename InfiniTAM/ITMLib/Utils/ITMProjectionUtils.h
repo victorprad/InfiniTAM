fc3e3eb90085c14c1e46d35268322e3479520fbb
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2016-08-17 11:21:51 +0100

    use templated vectors

--- InfiniTAM/ITMLib/Utils/ITMProjectionUtils.h
+++ InfiniTAM/ITMLib/Utils/ITMProjectionUtils.h
@@ -8,14 +8,13 @@
 #include "ITMMath.h"
 #include "../../ORUtils/PlatformIndependence.h"
 
-// Templated to handle both Vector3f and Vector4f as inputs
-template<typename T> _CPU_AND_GPU_CODE_ inline Vector2f project(const THREADPTR(T) &point_3d, const THREADPTR(Vector4f) &intrinsics)
+template<typename T> _CPU_AND_GPU_CODE_ inline Vector2f project(const THREADPTR(ORUtils::Vector3<T>) &point_3d, const THREADPTR(Vector4f) &intrinsics)
 {
 	return Vector2f(intrinsics.x * (float)point_3d.x / (float)point_3d.z + intrinsics.z,
 					intrinsics.y * (float)point_3d.y / (float)point_3d.z + intrinsics.w);
 }
 
-template<typename T> _CPU_AND_GPU_CODE_ inline Vector3f reproject(const THREADPTR(T) &point_2d, const THREADPTR(float) depth, const THREADPTR(Vector4f) &intrinsics)
+template<typename T> _CPU_AND_GPU_CODE_ inline Vector3f reproject(const THREADPTR(ORUtils::Vector2<T>) &point_2d, const THREADPTR(float) depth, const THREADPTR(Vector4f) &intrinsics)
 {
 	return Vector3f(depth * (((float)point_2d.x - intrinsics.z) / intrinsics.x),
 					depth * (((float)point_2d.y - intrinsics.w) / intrinsics.y),
