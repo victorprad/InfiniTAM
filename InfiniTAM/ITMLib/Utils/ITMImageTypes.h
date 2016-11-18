ffcfc20c2ffbad34610a79060b84c7d20e2b2736
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2016-11-18 10:39:02 +0000

    added more typedefs

--- InfiniTAM/ITMLib/Utils/ITMImageTypes.h
+++ InfiniTAM/ITMLib/Utils/ITMImageTypes.h
@@ -8,13 +8,17 @@
 typedef ORUtils::Image<bool> ITMBoolImage;
 typedef ORUtils::Image<float> ITMFloatImage;
 typedef ORUtils::Image<Vector2f> ITMFloat2Image;
+typedef ORUtils::Image<Vector3f> ITMFloat3Image;
 typedef ORUtils::Image<Vector4f> ITMFloat4Image;
 typedef ORUtils::Image<int> ITMIntImage;
+typedef ORUtils::Image<Vector2i> ITMInt2Image;
+typedef ORUtils::Image<Vector3i> ITMInt3Image;
+typedef ORUtils::Image<Vector4i> ITMInt4Image;
 typedef ORUtils::Image<short> ITMShortImage;
+typedef ORUtils::Image<Vector2s> ITMShort2Image;
 typedef ORUtils::Image<Vector3s> ITMShort3Image;
 typedef ORUtils::Image<Vector4s> ITMShort4Image;
 typedef ORUtils::Image<uchar> ITMUCharImage;
 typedef ORUtils::Image<Vector4u> ITMUChar4Image;
 typedef ORUtils::Image<uint> ITMUIntImage;
 typedef ORUtils::Image<ushort> ITMUShortImage;
-
