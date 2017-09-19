ea12a870f2e53288f17f88e7b78e68eabcb316a3
Tommaso Cavallari <tommaso.cavallari@unibo.it>
2017-09-19 15:53:28 +0100

    Allow setting multiple CUDA compute capabilities, for mixed-GPU systems

--- InfiniTAM/cmake/UseCUDA.cmake
+++ InfiniTAM/cmake/UseCUDA.cmake
@@ -49,6 +49,11 @@ IF(WITH_CUDA)
   IF(NOT MSVC_IDE)
     SET(CUDA_NVCC_FLAGS -Xcudafe "--diag_suppress=cc_clobber_ignored" ; -Xcudafe "--diag_suppress=set_but_not_used" ; ${CUDA_NVCC_FLAGS})
   ENDIF()
+
+  # Set the compute capability flags.
+  FOREACH(compute_capability ${CUDA_COMPUTE_CAPABILITY})
+    LIST(APPEND CUDA_NVCC_FLAGS --generate-code arch=compute_${compute_capability},code=sm_${compute_capability})
+  ENDFOREACH()
 ELSE()
   ADD_DEFINITIONS(-DCOMPILE_WITHOUT_CUDA)
 ENDIF()
