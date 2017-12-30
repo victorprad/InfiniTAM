169b7d6d8d76bc3e5369e9940b96cbef7ee48596
Vivien Miniussi <vivien.miniussi@gmail.com>
2017-12-30 16:42:00 +0100

    code style: indent using tabs to match existing practices of infiniTAM

--- InfiniTAM/InputSource/RealSense2Engine.h
+++ InfiniTAM/InputSource/RealSense2Engine.h
@@ -7,26 +7,27 @@
 namespace rs2 { class pipeline; class context; class device; }
 
 namespace InputSource {
-
-    class RealSense2Engine : public BaseImageSourceEngine
-    {
-    private:
-        bool dataAvailable;
-        std::unique_ptr<rs2::context> ctx;
-        std::unique_ptr<rs2::device> device;
-        std::unique_ptr<rs2::pipeline> pipe;
-        
-        Vector2i imageSize_rgb, imageSize_d;
-        
-    public:
-        RealSense2Engine(const char *calibFilename, bool alignColourWithDepth = true,
-                         Vector2i imageSize_rgb = Vector2i(640, 480), Vector2i imageSize_d = Vector2i(640, 480));
-        ~RealSense2Engine();
-        
-        bool hasMoreImages(void) const;
-        void getImages(ITMUChar4Image *rgb, ITMShortImage *rawDepth);
-        Vector2i getDepthImageSize(void) const;
-        Vector2i getRGBImageSize(void) const;
-    };
-
+	
+	class RealSense2Engine : public BaseImageSourceEngine
+	{
+	private:
+		bool dataAvailable;
+		std::unique_ptr<rs2::context> ctx;
+		std::unique_ptr<rs2::device> device;
+		std::unique_ptr<rs2::pipeline> pipe;
+		
+		Vector2i imageSize_rgb, imageSize_d;
+		
+	public:
+		RealSense2Engine(const char *calibFilename, bool alignColourWithDepth = true,
+						 Vector2i imageSize_rgb = Vector2i(640, 480), Vector2i imageSize_d = Vector2i(640, 480));
+		~RealSense2Engine();
+		
+		bool hasMoreImages(void) const;
+		void getImages(ITMUChar4Image *rgb, ITMShortImage *rawDepth);
+		Vector2i getDepthImageSize(void) const;
+		Vector2i getRGBImageSize(void) const;
+	};
+	
 }
+
